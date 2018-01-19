eb9f71a53ea973cee4a2f13d49c3addcdcc4eb92
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-01-19 14:24:49 -0500

    fixing syntax errors

--- 00-skill-template/lambda/index.js
+++ 00-skill-template/lambda/index.js
@@ -1,7 +1,7 @@
 var Alexa = require('alexa-sdk');
 
 
-var alexaMeetups = require("./data/alexaMeetups")
+var alexaMeetups = require("./data/alexaMeetups");
 
 exports.handler = function(event, context, callback){
   var alexa = Alexa.handler(event, context);
@@ -17,11 +17,11 @@ var handlers = {
 
   'Hello': function () {
     this.emit(':tell', 'Hello Hi there');
-  }
+  },
 
 'AlexaMeetupNumbers' : function() {
   var meetupNumbers = alexaMeetups.length;
   console.log("meetupNumbers->"+meetupNumbers);
-    this.emit(':ask', 'There are totally ${meetupNumbers} meetups. Check to see if your city is one of them!', 'How can I help ?');
+  this.emit(':ask', `There are currently ${meetupNumbers} Alexa developer meetups. Check to see if your city is one of them!`, 'How can i help?');
 }
 };
