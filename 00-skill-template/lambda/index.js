bb9c862fc7c5a3b25f095ef981f16375b23d4025
Vigneshwaran Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2018-01-19 16:02:32 -0500

    check city for meetup

--- 00-skill-template/lambda/index.js
+++ 00-skill-template/lambda/index.js
@@ -23,5 +23,42 @@ var handlers = {
   var meetupNumbers = alexaMeetups.length;
   console.log("meetupNumbers->"+meetupNumbers);
   this.emit(':ask', `There are currently ${meetupNumbers} Alexa developer meetups. Check to see if your city is one of them!`, 'How can i help?');
+},
+'AlexaMeetupCityCheck' : function() {
+  var USCitySlot = this.event.request.intent.slots.USCity.value;
+  var EuropeCitySlot = this.event.request.intent.slots.EuropeCity.value;
+
+
+  var city;
+  if(USCitySlot){
+    city=USCitySlot;
+  }
+  else if (EuropeCitySlot){
+    city = EuropeCitySlot;
+  }
+  else{
+    this.emit(':ask',"Sorry I didnt recongnize the city name", "How can I help?")
+  }
+
+
+
+var cityMatch = "";
+
+for(var i=0;i<alexaMeetups.length;i++){
+  if(alexaMeetups[i].city.toLowerCase()=== city.toLowerCase()){
+    cityMatch = alexaMeetups[i].city;
+  }
+}
+
+
+
+if(cityMatch !== "")
+{
+  this.emit(':ask', `Yes  ${city} has an Alexa developer meetup! `, 'How can i help?');
+}
+else{
+  this.emit(':ask', `Sorry, we dont have meetup in your city  ${city}. `, 'How can i help?');
+}
+
 }
 };
